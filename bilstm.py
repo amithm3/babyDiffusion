8bbb1b0b6a0a5ef4a7a3dd08cc31605254ac1e04
Gautam Menon <menongautam013@gmail.com>
2023-08-20 18:40:34 +0530

    Bidirectional LSTM

--- bilstm.py
+++ bilstm.py
@@ -2,18 +2,19 @@ import torch
 from torch import nn
 
 
-class LSTMEncoder(nn.Module):
+class BiLSTMEncoder(nn.Module):
     def __init__(self,
                  vocab_size: int,
                  embedding_dim: int,
                  hidden_dim: int,
                  num_layers: int,
-                 p: float = 0.2):
+                 p: float = 0.2,
+                 bid: bool = False):
         super().__init__()
 
         self._dropout = nn.Dropout(p)
         self._embedding = nn.Embedding(vocab_size, embedding_dim)
-        self._lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, dropout=p)
+        self._lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, dropout=p, bidirectional=bid)
 
     def forward(self, x):
         embedded = self._dropout(self._embedding(x))
@@ -21,19 +22,20 @@ class LSTMEncoder(nn.Module):
         return hidden, cell
 
 
-class LSTMDecoder(nn.Module):
+class BiLSTMDecoder(nn.Module):
     def __init__(self,
                  vocab_size: int,
                  embedding_dim: int,
                  hidden_dim: int,
                  num_layers: int,
-                 p: float = 0.2):
+                 p: float = 0.2,
+                 bid: bool = False):
         super().__init__()
 
         self._dropout = nn.Dropout(p)
         self._embedding = nn.Embedding(vocab_size, embedding_dim)
-        self._lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, dropout=p)
-        self._fc = nn.Linear(hidden_dim, vocab_size)
+        self._lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers, dropout=p, bidirectional=bid)
+        self._fc = nn.Linear(hidden_dim * 2, vocab_size)
 
     def forward(self, x, hidden, cell):
         x = x.unsqueeze(0)
@@ -43,18 +45,19 @@ class LSTMDecoder(nn.Module):
         return prediction, hidden, cell
 
 
-class LSTMAutoEncoder(nn.Module):
+class BiLSTMAutoEncoder(nn.Module):
     def __init__(self,
                  tokenizer: "Tokenizer",
                  embedding_dim: int = 128,
                  hidden_dim: int = 1024,
                  num_layers: int = 2,
-                 p: float = 0.2):
+                 p: float = 0.2,
+                 bid: bool = False):
         super().__init__()
 
         self.tokenizer = tokenizer
-        self._encoder = LSTMEncoder(len(tokenizer), embedding_dim, hidden_dim, num_layers, p)
-        self._decoder = LSTMDecoder(len(tokenizer), embedding_dim, hidden_dim, num_layers, p)
+        self._encoder = BiLSTMEncoder(len(tokenizer), embedding_dim, hidden_dim, num_layers, p, bid)
+        self._decoder = BiLSTMDecoder(len(tokenizer), embedding_dim, hidden_dim, num_layers, p, bid)
 
     def forward(self, source, tfr=0.5):
         batch_size = source.shape[1]
